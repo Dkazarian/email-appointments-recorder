dbccbc8fc1f1c6d5d355ecf8b505714fa87f4d21
Daniela Kazarian <danielakazarian@gmail.com>
2026-08-02 03:50:42 +0100

    refactor: remove obsolete Ollama client

--- app/ia_clients/__init__.py
+++ app/ia_clients/__init__.py
@@ -1,12 +1,10 @@
 from .gemini import GeminiIAClient
 from .lm_studio import LocalIAClient, LocalIAError
-from .ollama import OllamaError
 from .openrouter import OpenRouterError, OpenRouterIAClient
 
 __all__ = [
     "GeminiIAClient",
     "LocalIAError",
-    "OllamaError",
     "OpenRouterError",
     "OpenRouterIAClient",
     "LocalIAClient",
